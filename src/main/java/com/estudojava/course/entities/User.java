1974aa8caef0d3bd659d0d772e691d95cb1e278a
Natan Dadalt <natan.db@gmail.com>
2023-02-18 21:55:30 -0300

    Revert "Order, Instant, ISO 8601 - Part 1"
    This reverts commit 6079a7931c242d9d7fd33d5b84fe5f6927b69521.

--- src/main/java/com/estudojava/course/entities/User.java
+++ src/main/java/com/estudojava/course/entities/User.java
@@ -1,15 +1,12 @@
 package com.estudojava.course.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -26,16 +23,8 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
 
 	/* Constructors */
-	
-	public List<Order> getOrders() {
-		return orders;
-	}
-	
 	public User() {
 
 	}
