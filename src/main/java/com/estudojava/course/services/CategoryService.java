2aa42358d5eed07412ce0c45f2509a3766e6558b
Natan Dadalt <natan.db@gmail.com>
2023-02-19 21:41:29 -0300

    Category

---
+++ src/main/java/com/estudojava/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.estudojava.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudojava.course.entities.Category;
+import com.estudojava.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository; 
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
